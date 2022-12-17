ec55c91af70a97323410fc594052f53badd95aeb
Jafa Mohammed <jmohsa@gmail.com>
2022-12-17 10:56:35 +0300

    Modified dto

--- src/main/java/com/example/stocktrade/dto/User.java
+++ src/main/java/com/example/stocktrade/dto/User.java
@@ -1,2 +1,11 @@
-package com.example.stocktrade.dto;public class User {
-}
+package com.example.stocktrade.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public record User (
+    Long id,
+    @NotEmpty(message = "Name is required")
+    String username
+)
+
+{}
